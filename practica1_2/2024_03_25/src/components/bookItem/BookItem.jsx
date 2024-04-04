08b36ce6e7aeb30d174c62713e16fcdcfe56b7e8
Pablo Macieyko <macieyko.pablo@gmail.com>
2024-04-04 10:34:48 -0300

    run from place

---
+++ practica1_2/2024_03_25/src/components/bookItem/BookItem.jsx
@@ -0,0 +1,23 @@
+import "./BookItem.css";
+import PropTypes from 'prop-types';
+import { Card } from "react-bootstrap";
+
+const BookItem = ({ title, author, pageCount, rating, imageUrl }) => {
+  return (
+    <Card style={{ width: "22rem" }}>
+      <Card.Img
+        height={400}
+        variant="top"
+        src={imageUrl !== "" ? imageUrl : "https://bit.ly/47NylZk"}
+      />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Subtitle>{author}</Card.Subtitle>
+        <div>{rating?.length} estrellas</div>
+        <p>{pageCount} páginas</p>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default BookItem;
