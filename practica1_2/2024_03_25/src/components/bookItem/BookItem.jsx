3dfa3a038537ebb212b790eed64a1a0dcd7b136c
Pablo Macieyko <macieyko.pablo@gmail.com>
2024-04-12 14:08:51 -0300

    added css in BookItem and Book. Bootstrap working Books.jsx fixed

--- practica1_2/2024_03_25/src/components/bookItem/BookItem.jsx
+++ practica1_2/2024_03_25/src/components/bookItem/BookItem.jsx
@@ -1,19 +1,20 @@
-import "./BookItem.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { Card } from "react-bootstrap";
+import "./BookItem.css";
+
 
 const BookItem = ({ title, author, pageCount, rating, imageUrl }) => {
   return (
-    <Card className="mx-3" style={{ width: "22rem" }}>
+    <Card className="mx-1" style={{ width: "22rem" }}>
       <Card.Img
         height={400}
         variant="top"
         src={imageUrl !== "" ? imageUrl : "https://bit.ly/47NylZk"}
       />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Subtitle>{author}</Card.Subtitle>
-        <div>{rating?.length} estrellas</div>
+        <Card.Title> Titulo {title}</Card.Title>
+        <Card.Subtitle> Author {author}</Card.Subtitle>
+        <div>estrellas {rating?.length} </div>
         <p>{pageCount} páginas</p>
       </Card.Body>
     </Card>
@@ -25,7 +26,6 @@ BookItem.propTypes = {
   author: PropTypes.string,
   pageCount: PropTypes.number,
   rating: PropTypes.array,
-  };
-  
+};
 
 export default BookItem;
--- practica1_2/2024_03_25/src/components/books/Books.jsx
+++ practica1_2/2024_03_25/src/components/books/Books.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import BookItem from "../bookItem/BookItem";
-
-
+import "./Books.css";
 
 const Books = ({ books }) => {
-    return (
-        <div className="d-flex justify-content-center flex-wrap">
-            {books.map((book, index) => (
-                <BookItem
-                    key={index}
-                    title={book.title}
-                    author={book.author}
-                    pageCount={book.pageCount}
-                    rating={book.rating}
-                    imageUrl={book.imageUrl}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className="d-flex justify-content-center flex-wrap">
+      {books.map((book, index) => (
+        <BookItem
+          key={index}
+          title={book.bookTitle}
+          author={book.bookAuthor}
+          pageCount={book.pageCount}
+          rating={book.bookRating}
+          imageUrl={book.imageUrl}
+        />
+      ))}
+    </div>
+  );
 };
 
 Books.propTypes = {
-    title: PropTypes.string,
-    author: PropTypes.string,
-    pageCount: PropTypes.number,
-    rating: PropTypes.array,
+  title: PropTypes.string,
+  author: PropTypes.string,
+  pageCount: PropTypes.number,
+  rating: PropTypes.array,
 };
 
 export default Books;
